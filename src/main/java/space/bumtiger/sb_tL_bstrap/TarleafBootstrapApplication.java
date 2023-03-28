9450a68e4064e208a1386fb396ec918b2ab5e1eb
osparking <jbpark03@gmail.com>
2023-03-28 13:15:21 +0900

    springboot thymeleaf bootstrap 5 init

---
+++ src/main/java/space/bumtiger/sb_tL_bstrap/TarleafBootstrapApplication.java
@@ -0,0 +1,13 @@
+package space.bumtiger.sb_tL_bstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarleafBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarleafBootstrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumtiger/sb_tL_bstrap/TarleafBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumtiger.sb_tL_bstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarleafBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
